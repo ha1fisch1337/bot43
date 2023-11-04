da97aefc1cbbc7e028a3dd0e6af12e63e8bd5ebf
ha1fisch1337 <89041784+ha1fisch1337@users.noreply.github.com>
2023-11-04 03:36:59 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,10 +1,13 @@
 from aiogram import Bot, Dispatcher, executor, types
+from aiogram.types import InputMediaAudio
 from key import key
 from pytube import YouTube
 from moviepy.editor import *
+from pydub import AudioSegment
 
 
 API_TOKEN = key
+l=3_000_000
 
 bot = Bot(token=API_TOKEN)
 dp = Dispatcher(bot)
@@ -25,11 +28,23 @@ def f(link):
 
     video = VideoFileClip(f"{name}.mp4")
     video.audio.write_audiofile(f"{name}.mp3")
-    return f"{name}.mp3"
+    sound = AudioSegment.from_mp3(f"{name}.mp3")
+    if len(sound)//l>1:
+        for i in range(len(sound)//l+2):
+            n=i+1
+            if i==len(sound)//l+1:
+                sound[(i+1)*l::].export(f"Audio{n}.mp3",format="mp3")
+            else:
+                sound[l*i:l*(i+1)].export(f"Audio{n}.mp3",format="mp3")
+        media=[f"Audio{i}.mp3" for i in range(1,n+1)]
+    else:
+        media="Audio.mp3"
+    return media
 
 @dp.message_handler()
 async def echo(message: types.Message):
-    await bot.send_audio(message.from_user.id,audio=open(f(message.text),"rb"))
+    for i in f(message.text):
+        await bot.send_audio(message.from_user.id, open(i,"rb"))
 
 
 
